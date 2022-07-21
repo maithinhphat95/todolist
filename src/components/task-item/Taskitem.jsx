4ac3ac4d8d3f92c409d4598c019666fe82ef52bb
Phat-Mai <maithinhphat.tdh@gmail.com>
2022-07-21 23:18:09 +0700

    update todolist

---
+++ src/components/task-item/Taskitem.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { useState } from "react";
+import { propTypes } from "react-bootstrap/esm/Image";
+
+Taskitem.propTypes = {
+  taskName: PropTypes.string,
+  authorName: PropTypes.string,
+  time: PropTypes.number,
+  taskItem: PropTypes.object,
+};
+Taskitem.defaultProps = {
+  taskName: "Task name Original",
+};
+function Taskitem(props) {
+  const { taskName, authorName, time } = props;
+  const [status, setStatus] = useState("Start");
+  // Init the object that has all information
+
+  return (
+    <div className="task-item">
+      <p>Task title: {taskName}</p>
+      <p>Author: {authorName}</p>
+      <p>Time: {time}</p>
+      <p>Status: {status}</p>
+      <button onClick={() => setStatus("Start")}>Start</button>
+      <button onClick={() => setStatus("Done")}>Done</button>
+      <button onClick={() => setStatus("Doing")}>Doing</button>
+      <button onClick={() => setStatus("Cancel")}>Cancel</button>
+    </div>
+    //   New card 2
+  );
+}
+export default Taskitem;
