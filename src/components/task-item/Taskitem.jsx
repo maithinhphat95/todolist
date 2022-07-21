487f4c378f29b35e81c139d50ebe6103c328270f
Phat-Mai <maithinhphat.tdh@gmail.com>
2022-07-21 23:22:25 +0700

    update react

--- src/components/task-item/Taskitem.jsx
+++ src/components/task-item/Taskitem.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { useState } from "react";
-import { propTypes } from "react-bootstrap/esm/Image";
 
 Taskitem.propTypes = {
   taskName: PropTypes.string,
