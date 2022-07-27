03ab23eabf539141f48be75487d96ad451352631
Phat-Mai <maithinhphat.tdh@gmail.com>
2022-07-28 05:45:46 +0700

    update the view

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -17,34 +17,6 @@ function TaskList(props) {
           taskDescript={element.descript}
         />
       ))}
-      {/* <Taskitem taskTitle="Morning - Go to Work" taskCreator="Phat" />
-      <Taskitem
-        taskTitle="Mid-day - Working"
-        taskCreator="Phat"
-        taskDescript="Do the job at the company"
-      />
-      <Taskitem
-        taskTitle="Afternoon - Working"
-        taskCreator="Phat"
-        taskDescription="Do the job at the company, submit the daily report"
-      />
-      <Taskitem
-        taskTitle="Early-night - Study"
-        taskCreator="Phat"
-        taskDescription="Learning FE25, meet the pretty teacher"
-      />
-      <Taskitem
-        taskTitle="Night - Study"
-        taskCreator="Phat"
-        taskDescription="Review the slide, do the Excersice"
-      />
-      <Taskitem
-        taskTitle="Late-night - Sleep"
-        taskCreator="Phat"
-        taskDescription="Have a nice dream"
-      />
-      <Taskitem />
-      <Taskitem /> */}
     </div>
   );
 }
